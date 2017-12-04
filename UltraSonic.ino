79d414d74bb00ea7a3bd5623502aa4c015f8763a
JangoMV <dylan.endries@gmail.com>
2017-12-04 14:53:12 -0600

    UltraSonic v0.1
    Ultra-sonic obstacle detection.
    v0.1 changelog:
    -created
    
    TODO:
    -determine proper detection distance
    -eventually, merge with MasterControl

---
+++ UltraSonic.ino
@@ -0,0 +1,34 @@
+const int pingPin = 7;
+boolean temp_bool = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  long duration, inches;
+  pinMode(pingPin,OUTPUT);
+  digitalWrite(pingPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin,High);
+  delayMicroseconds(5);
+  digitalWrite(pingPin,LOW);
+
+  pinMode(pingPin,INPUT);
+  duration = pulseIn(pingPin,HIGH);
+
+  inches = duration / 74 / 2;
+
+  if (inches > 5)
+  {
+    temp_bool = 0;
+    Serial.println("No Obstructions Detected");
+  }
+  else
+  {
+    temp_bool = 1;
+    Serial.println("Obstruction Detected");
+  }
+}
